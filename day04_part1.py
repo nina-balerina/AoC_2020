6df00d15ca643f96c12ce53f40eb218e2e4c0102
Nina Ceh <71320289+nina-balerina@users.noreply.github.com>
2020-12-15 15:36:49 +0100

    Update day04_part1.py

--- day04_part1.py
+++ day04_part1.py
@@ -8,32 +8,23 @@ import time
 start_time = time.time()
 
 f = open("day4.txt",'r')
-txt = f.readlines()
+txt = f.read()
+in1 = txt.split('\n\n')
 
 fields = {'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid', 'cid'}
 ok_fields = {'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'}
 
-i = 0; k = 0
-a = {}
 valid = 0
-for line in txt:
-    if line in ['\n', '\r\n']:
-        for f in ok_fields:
-            if f in a:
-                k +=1
-        if k==7:
-            valid +=1
-            print(a, valid)
-        i = 0; k = 0
-        a = {}
-    else:
-        i +=1
-        print(i)
-        bs = line.strip().split(' ')
-        cs = []
-        j = 0
-        for b in bs:
-            cs.append(b.split(':'))
-            a[cs[j][0]] = cs[j][1]
-            j +=1
-    
+for line in in1:
+    k = 0
+    a = {}
+    bs = line.replace('\n',' ').strip().split(' ')
+    for b in bs:
+        cs = b.split(':')
+        a[cs[0]] = cs[1]
+    for f in ok_fields:
+        if f in a: k +=1
+    if k==7: valid +=1
+
+print("part1 : %s" % valid)
+print("--- %s seconds ---" % (time.time() - start_time))  
